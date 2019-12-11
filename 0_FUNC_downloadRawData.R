946c2e3719a1727c984bef9e22d268d7a1af32e4
MayaGueguen <maya.gueguen@gmail.com>
2019-12-11 11:15:15 +0100

    Correct future scenario for future climate data.

--- 0_FUNC_downloadRawData.R
+++ 0_FUNC_downloadRawData.R
@@ -12,14 +12,25 @@ setwd(path.to.data)
 list.mm = c(paste0("0", 1:9), 10:12)
 past.years = 1979:2013
 
+fut.scenarios = c("ACCESS1-0", "BNU-ESM", "CCSM4"
+                  , "CESM1-BGC", "CESM1-CAM5", "CMCC-CMS", "CMCC-CM", "CNRM-CM5"
+                  , "CSIRO-Mk3-6-0", "CSIRO-Mk3L-1-2", "CanESM2"
+                  , "EC-EARTH", "FGOALS-g2", "FIO-ESM", "GFDL-CM3", "GFDL-ESM2G", "GFDL-ESM2M"
+                  , "GISS-E2-H-CC", "GISS-E2-H", "GISS-E2-R-CC", "GISS-E2-R"
+                  , "HadGEM2-AO", "HadGEM2-CC", "HadGEM2-ES"
+                  , "IPSL-CM5A-LR", "IPSL-CM5A-MR", "MIROC-ESM-CHEM", "MIROC-ESM", "MIROC5"
+                  , "MPI-ESM-LR", "MPI-ESM-MR", "MRI-CGCM3", "MRI-ESM1"
+                  , "NorESM1-M", "bcc-csm1-1", "inmcm4", "")
 fut.scenarios = c("CESM1-BGC", "IPSL-CM5A-MR", "MPI-ESM-LR")
+fut.scenarios = c("CMCC-CM", "ACCESS1-0", "MIROC5", "CESM1-BGC")
 fut.rcp = c("45", "85")
 fut.years = c("2041-2060", "2061-2080")
 
 fut.ts.scenarios1 = c("cmcc_cmcc-cm_ar5", "csiro-bom_access1-3_ar5", "miroc_miroc5_ar5", "nsf-doe-ncar_cesm1-bgc_ar5")
-fut.ts.scenarios2 = c("CESM1-BGC", "IPSL-CM5A-MR", "MIROC5", "CESM1-BGC")
+fut.ts.scenarios2 = c("CMCC-CM", "ACCESS1-3", "MIROC5", "CESM1-BGC")
 fut.ts.rcp = c("45", "85")
-fut.ts.years = c("2041-2060", "2061-2080")
+fut.ts.years = c(2010:2019, seq(2020, 2100, 10))
+fut.ts.years = seq(2010, 2100, 10)
 
 
 ###################################################################
@@ -41,9 +52,11 @@ path.CHELSA.timeseries.tmax = paste0(path.CHELSA.timeseries, "tmax/")
 path.CHELSA.timeseries.fut = paste0(path.CHELSA, "cmip5_ts/")
 
 dir.create(paste0(path.to.data, "PRECIPITATION/RAW/"), recursive = TRUE)
+dir.create(paste0(path.to.data, "PRECIPITATION/RAW_FUTURE/"), recursive = TRUE)
 dir.create(paste0(path.to.data, "PRECIPITATION/RAW_TS_PAST/"), recursive = TRUE)
 dir.create(paste0(path.to.data, "PRECIPITATION/RAW_TS_FUTURE/"), recursive = TRUE)
 dir.create(paste0(path.to.data, "TEMPERATURE/RAW/"), recursive = TRUE)
+dir.create(paste0(path.to.data, "TEMPERATURE/RAW_FUTURE/"), recursive = TRUE)
 dir.create(paste0(path.to.data, "TEMPERATURE/RAW_TS_PAST/"), recursive = TRUE)
 dir.create(paste0(path.to.data, "TEMPERATURE/RAW_TS_FUTURE/"), recursive = TRUE)
 
@@ -68,15 +81,29 @@ foreach(mm = combi$mm, ye = combi$ye) %dopar%
   }
 
 ## FUTURE CLIMATOLOGIES
-combi = expand.grid(mm = list.mm, sce = fut.scenarios, rcp = fut.rcp, ye = fut.years)
+combi = expand.grid(mm = as.numeric(list.mm), sce = fut.scenarios, rcp = fut.rcp, ye = fut.years)
 foreach(mm = combi$mm, sce = combi$sce, rcp = combi$rcp, ye = combi$ye) %do%
   {
-    system(paste0("wget -P ", path.to.data, "PRECIPITATION/RAW/ ", path.CHELSA.climatologies.fut, ye
+    system(paste0("wget -P ", path.to.data, "PRECIPITATION/RAW_FUTURE/ ", path.CHELSA.climatologies.fut, ye
                   , "/prec/CHELSA_pr_mon_", sce, "_rcp", rcp, "_r1i1p1_g025.nc_", mm, "_", ye, ".tif"))
-    system(paste0("wget -P ", path.to.data, "TEMPERATURE/RAW/ ", path.CHELSA.climatologies.fut, ye
-                  , "/prec/CHELSA_tasmin_mon_", sce, "_rcp", rcp, "_r1i1p1_g025.nc_", mm, "_", ye, "_V1.2.tif"))
-    system(paste0("wget -P ", path.to.data, "TEMPERATURE/RAW/ ", path.CHELSA.climatologies.fut, ye
-                  , "/prec/CHELSA_tasmax_mon_", sce, "_rcp", rcp, "_r1i1p1_g025.nc_", mm, "_", ye, "_V1.2.tif"))
+    system(paste0("wget -P ", path.to.data, "TEMPERATURE/RAW_FUTURE/ ", path.CHELSA.climatologies.fut, ye
+                  , "/tmin/CHELSA_tasmin_mon_", sce, "_rcp", rcp, "_r1i1p1_g025.nc_", mm, "_", ye, "_V1.2.tif"))
+    system(paste0("wget -P ", path.to.data, "TEMPERATURE/RAW_FUTURE/ ", path.CHELSA.climatologies.fut, ye
+                  , "/tmax/CHELSA_tasmax_mon_", sce, "_rcp", rcp, "_r1i1p1_g025.nc_", mm, "_", ye, "_V1.2.tif"))
+  }
+
+## FUTURE TIMESERIES
+combi = expand.grid(mm = list.mm, i.sce = length(fut.ts.scenarios1), rcp = fut.ts.rcp, ye = fut.ts.years)
+foreach(mm = combi$mm, i.sce = combi$i.sce, rcp = combi$rcp, ye = combi$ye) %do%
+  {
+    cat("\n ==> Scenario ", fut.ts.scenarios2[i.sce], " year ", ye, " month ",  mm, " rcp ", rcp)
+    
+    system(paste0("wget -P ", path.to.data, "TEMPERATURE/RAW_TS_FUTURE/ ", path.CHELSA.timeseries.fut
+                  , "/", fut.ts.scenarios1[i.sce], "/CHELSA_", fut.ts.scenarios2[i.sce]
+                  , "_rcp", rcp, "_", ye, "_", mm, "_tmin.tif"))
+    system(paste0("wget -P ", path.to.data, "TEMPERATURE/RAW_TS_FUTURE/ ", path.CHELSA.timeseries.fut
+                  , "/", fut.ts.scenarios1[i.sce], "/CHELSA_", fut.ts.scenarios2[i.sce]
+                  , "_rcp", rcp, "_", ye, "_", mm, "_tmax.tif"))
   }
 
 ###################################################################
