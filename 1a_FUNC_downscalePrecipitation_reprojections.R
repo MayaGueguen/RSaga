dd024f07ba6c323b16f7c32bb12b5832aea205ca
MayaGueguen <maya.gueguen@gmail.com>
2020-11-26 14:32:33 +0100

    Update reprojections of precipitations

--- 1a_FUNC_downscalePrecipitation_reprojections.R
+++ 1a_FUNC_downscalePrecipitation_reprojections.R
@@ -6,33 +6,23 @@ library(raster)
 library(rgdal)
 
 # path.to.data = "C:/Users/gueguen/Documents/CLIMATE_DOWNSCALING/"
-# path.to.SAGA = "C:/Program Files (x86)/SAGA-GIS/"
-path.to.data = "/run/user/1001/gvfs/smb-share:server=129.88.191.70,share=equipes/macroeco/GIS_DATA/CHELSA_DOWNSCALING/"
-path.to.data = "/home/gueguema/Documents/CHELSA_DOWNSCALING/"
-path.to.SAGA = path.to.data
+path.to.SAGA = "C:/Program Files (x86)/SAGA-GIS/"
+# path.to.data = "/media/gueguen/equipes/emabio/GIS_DATA/CHELSA_DOWNSCALING/"
+# path.to.data = "/bettik/mayagueguen/CHELSA_DOWNSCALING/"
+path.to.data = "K:/LECA/macroeco/GIS_DATA/CHELSA_DOWNSCALING/"
+# path.to.SAGA = path.to.data
 
 zone_name.precip = "World"
-proj.res.precip = 850
+proj.res.precip = 1200
 proj.name = "ETRS89"
 proj.value = "+proj=laea +lat_0=52 +lon_0=10 +x_0=4321000 +y_0=3210000 +ellps=GRS80 +units=m +no_defs "
 
-list.mm = c(paste0("0", 1:9), 10:12)
-past.years = 1979:2013
-
-fut.scenarios = c("CMCC-CM", "ACCESS1-0", "MIROC5", "CESM1-BGC")
+fut.scenarios = c("CESM1-BGC", "IPSL-CM5A-MR", "MPI-ESM-LR")
 fut.rcp = c("45", "85")
-fut.years = c("2041-2060", "2061-2080")
-
-fut.ts.scenarios = c("CMCC-CM", "ACCESS1-3", "MIROC5", "CESM1-BGC")
-fut.ts.rcp = c("45", "85")
-fut.ts.years = seq(2010, 2100, 10)
+fut.years = c("2061-2080")
 
 setwd(path.to.SAGA)
 
-### !!!!
-## PROBLEM : Code seems fine, but reprojected maps are cut
-## They have be done separately with ARCGIS
-### !!!!
 
 ###################################################################
 ### REPROJECT INPUT data (must be EQUAL AREA projection !!)
@@ -51,7 +41,7 @@ if (!dir.exists(paste0(path.to.data, "PRECIPITATION/RAW/", new.folder.name)))
 for (mm in 1:12)
 {
   cat("\n ==> Reproject CHELSA precipitations for month ", mm, "\n")
-
+  
   input.name = paste0("PRECIPITATION/RAW/CHELSA_prec_", mm, "_V1.2_land.tif")
   new.file.name = paste0("PRECIP_", zone_name.precip, "_", proj.name, "_resolution", proj.res.precip, "_", mm, ".sgrd")
   output.name = sub(
@@ -59,26 +49,18 @@ for (mm in 1:12)
     paste0(new.folder.name, new.file.name),
     input.name
   )
-
+  
   if (!file.exists(paste0(path.to.data, output.name)))
   {
-    # system.command = paste0("saga_cmd grid_tools 12 -INPUT="
-    #                         , paste0("\"", path.to.data, input.name, "\"")
-    #                         , " -OUTPUT="
-    #                         , paste0("\"", path.to.data, output.name, "\"")
-    #                         , " -METHOD=0 -IDENTITY=\"new_param.txt\"")
-    # 
-    # system(system.command)
-    # 
-    # system.command = paste0("saga_cmd pj_proj4 3 -CRS_PROJ4="
-    #                         , paste0("\"", proj.value, "\"")
-    #                         , " -SOURCE="
-    #                         , paste0("\"", path.to.data, output.name, "\"")
-    #                         , " -GRIDS="
-    #                         , paste0("\"", path.to.data, output.name, "\"")
-    #                         , " -RESAMPLING=3") ## B-spline interpolation
-    # 
-    # system(system.command)
+    system.command = paste0("saga_cmd pj_proj4 3 -CRS_PROJ4="
+                            , paste0("\"", proj.value, "\"")
+                            , " -SOURCE="
+                            , paste0("\"", path.to.data, input.name, "\"")
+                            , " -GRIDS="
+                            , paste0("\"", path.to.data, output.name, "\"")
+                            , " -RESAMPLING=3") ## B-spline interpolation
+    
+    system(system.command)
   }
 }
 
@@ -94,39 +76,39 @@ if (!dir.exists(paste0(path.to.data, "PRECIPITATION/RAW/", new.folder.name)))
   dir.create(paste0(path.to.data, "PRECIPITATION/RAW/", new.folder.name))
 }
 
-# for (sce in fut.scenarios)
-# {
-#   for (rcp in fut.rcp)
-#   {
-#     for (ye in fut.years)
-#     {
-#       ### Monthly precipitations
-#       for (mm in 1:12)
-#       {
-#         cat("\n ==> Reproject CHELSA precipitations for month ", mm, "\n")
-#         
-#         input.name = paste0("PRECIPITATION/RAW/FUTURE/CHELSA_pr_mon_", sce, "_rcp", rcp, "_r1i1p1_g025.nc_", mm, "_", ye, ".tif")
-#         new.file.name = paste0("PRECIP_", zone_name.precip, "_", proj.name, "_resolution", proj.res.precip, "_"
-#                                , sce, "_rcp", rcp, "_", mm, "_", ye, ".sgrd")
-#         output.name = sub(
-#           basename(input.name),
-#           paste0(new.folder.name, new.file.name),
-#           input.name
-#         )
-#         
-#         if (!file.exists(paste0(path.to.data, output.name)))
-#         {
-#           system.command = paste0("saga_cmd pj_proj4 3 -CRS_PROJ4="
-#                                   , paste0("\"", proj.value, "\"")
-#                                   , " -SOURCE="
-#                                   , paste0("\"", path.to.data, input.name, "\"")
-#                                   , " -GRIDS="
-#                                   , paste0("\"", path.to.data, output.name, "\"")
-#                                   , " -RESAMPLING=3") ## B-spline interpolation
-#           
-#           system(system.command)
-#         }
-#       }
-#     }
-#   }
-# }
+for (sce in fut.scenarios)
+{
+  for (rcp in fut.rcp)
+  {
+    for (ye in fut.years)
+    {
+      ### Monthly precipitations
+      for (mm in 1:12)
+      {
+        cat("\n ==> Reproject CHELSA precipitations for month ", mm, "\n")
+        
+        input.name = paste0("PRECIPITATION/RAW/FUTURE/CHELSA_pr_mon_", sce, "_rcp", rcp, "_r1i1p1_g025.nc_", mm, "_", ye, ".tif")
+        new.file.name = paste0("PRECIP_", zone_name.precip, "_", proj.name, "_resolution", proj.res.precip, "_"
+                               , sce, "_rcp", rcp, "_", mm, "_", ye, ".sgrd")
+        output.name = sub(
+          basename(input.name),
+          paste0("../", new.folder.name, new.file.name),
+          input.name
+        )
+        
+        if (!file.exists(paste0(path.to.data, output.name)))
+        {
+          system.command = paste0("saga_cmd pj_proj4 3 -CRS_PROJ4="
+                                  , paste0("\"", proj.value, "\"")
+                                  , " -SOURCE="
+                                  , paste0("\"", path.to.data, input.name, "\"")
+                                  , " -GRIDS="
+                                  , paste0("\"", path.to.data, output.name, "\"")
+                                  , " -RESAMPLING=3 -TARGET_USER_XMIN=(-8607000)") ## B-spline interpolation
+          
+          system(system.command)
+        }
+      }
+    }
+  }
+}
